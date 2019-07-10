fb3688a6e74bd91e63e9fb02e3eb885e5025eb7b
cyclowns <notzombiedude@gmail.com>
2019-07-09 18:59:37 -0700

    tests, outline, clap stuff
    Added some tests, changed the basic outline and functions of main.rs,
    and started working on stuff for clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+// TODO: Figure out unit testing stuff
+
+// TODO: Figure out a way to represent each brainfuck command
+// < > . , + - [ ]
+
+// TODO: Figure out how to represent the 'tape'
+// 30k length was used for the array in original BF, so lets do that
+
+#[cfg(test)]
+mod tests;
+
+#[allow(unused_imports)]
+use std::io::stdin;
+
+#[allow(dead_code)]
+/// Parses input into a stack (Vec) that is processed later
+fn parse_input() {
+
 }
+
+#[allow(dead_code)]
+fn run_bf_code() {
+
+}
+
+fn main() {
+
+}
+
