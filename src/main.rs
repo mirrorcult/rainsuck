ed48682cfbeb0b540efae1b9aac4a6a0b14bd448
cyclowns <notzombiedude@gmail.com>
2019-07-09 19:07:04 -0700

    command line stuff
    Adds clap, completes cli.yml, and adds boilerplate to main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-// TODO: Figure out unit testing stuff
+// TODO: Get command-line stuff working
+// -f (--file) arg for file input, -s (--string) arg for string input
+// -t (--textify) arg to take in text and produce BF code? maybe later?
 
 // TODO: Figure out a way to represent each brainfuck command
 // < > . , + - [ ]
@@ -6,11 +8,15 @@
 // TODO: Figure out how to represent the 'tape'
 // 30k length was used for the array in original BF, so lets do that
 
+#[macro_use]
+extern crate clap;
+
 #[cfg(test)]
 mod tests;
 
 #[allow(unused_imports)]
 use std::io::stdin;
+use clap::App;
 
 #[allow(dead_code)]
 /// Parses input into a stack (Vec) that is processed later
@@ -24,6 +30,7 @@ fn run_bf_code() {
 }
 
 fn main() {
-
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
 }
 
