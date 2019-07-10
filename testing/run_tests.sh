a9003e1d11fe636f2d237bdccebe1dbbabcfbf41
cyclowns <notzombiedude@gmail.com>
2019-07-10 00:24:29 -0700

    fix: testing now actually works

--- testing/run_tests.sh
+++ testing/run_tests.sh
@@ -1,5 +1,5 @@
-target/release/rainsuck -f beeronthewall.bf
-target/release/rainsuck -f helloworld.bf
-target/release/rainsuck -f sierpinski.bf
+target/release/rainsuck -f testing/beeronthewall.bf
+target/release/rainsuck -f testing/helloworld.bf
+target/release/rainsuck -f testing/sierpinski.bf
 
 target/release/rainsuck "++++++++++[>+>+++>+++++++>++++++++++<<<<-]>>>++.>+.+++++++..+++.<<++.>+++++++++++++++.>.+++.------.--------.<<+.<."
