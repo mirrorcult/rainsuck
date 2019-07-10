2f22a45e392873fea4692237e1979e62698d2031
cyclowns <notzombiedude@gmail.com>
2019-07-09 19:30:17 -0700

    clap actually works
    I hate clap's YML stuff. Why does it even exist, it never works?

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,6 @@ mod tests;
 
 #[allow(unused_imports)]
 use std::io::stdin;
-use clap::App;
 
 #[allow(dead_code)]
 /// Parses input into a stack (Vec) that is processed later
@@ -30,7 +29,13 @@ fn run_bf_code() {
 }
 
 fn main() {
-    let yaml = load_yaml!("cli.yml");
-    let matches = App::from_yaml(yaml).get_matches();
+    let matches = clap_app!(rainsuck =>
+        (version: "0.1.1")
+        (author: "cyclowns <cyclowns@protonmail.ch>")
+        (about: "Brainfuck interpreter for rust")
+        (@arg FILE: -f --file "Tells rainsuck that your input is a file")
+        (@arg STRING: -s --string "Tells rainsuck that your input is a string")
+        (@arg INPUT: +required "Brainfuck code to be interpreted, either a file or string as set by -f or -s")
+    ).get_matches();
 }
 
