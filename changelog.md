a45e6110d0befbd5532cca9d38f36860a58dd415
cyclowns <notzombiedude@gmail.com>
2019-07-09 21:07:10 -0700

    feat: add strip_chars and more stuff
    Finished strip_chars(), which deletes all non-brainfuck characters from
    a string of code

--- changelog.md
+++ changelog.md
@@ -6,6 +6,20 @@
 
 *   this is just a test ([59ed723f](https://github.com/cyclowns/rainsuck/commit/59ed723f3f1f7b66e55686ea24d8469f67bf82fe))
 
+#### Features
+
+*   some file reading stuff done ([b73b45f6](https://github.com/cyclowns/rainsuck/commit/b73b45f65944ad8410e18b7d975c1ef038e0fc25))
+
+
+
+<a name=""></a>
+##  (2019-07-10)
+
+
+#### Performance
+
+*   this is just a test ([59ed723f](https://github.com/cyclowns/rainsuck/commit/59ed723f3f1f7b66e55686ea24d8469f67bf82fe))
+
 
 
 <a name=""></a>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// TODO: Have parse_input remove
+
 // TODO: Figure out a way to represent each brainfuck command
 // < > . , + - [ ]
 
@@ -12,26 +14,67 @@ mod tests;
 
 #[allow(unused_imports)]
 use std::io::stdin;
+use std::io::Read;
+use std::path::Path;
+use std::fs::File;
 
 #[allow(dead_code)]
-/// Parses input into a stack (Vec) that is processed later
-fn parse_input() {
+/// Parses a snippet of BF code and evaluates it
+/// ```rust
+/// let p = evaluate_brainfuck(String::new("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+."))
+/// println!({}, p)
+/// // p = "Hello World!"
+/// ```
+fn evaluate_brainfuck(mut code: String) {
+    let memory: [i32; 30000] = [0; 30000]; // zero-initialize every cell
+    let pointer: i32 = 0; // pointer starts at 0
+
+    code = strip_chars(code); // strips a-zA-Z, etc everything not used in brainfuck
+    for c in code.chars() {
+
+    }
+}
+
+/// Sets up a vector containing a list of each [] bracket's
+/// pairing, to make evaluating them easier.
+fn setup_bracket_mapping(code: String) {
 
 }
 
-#[allow(dead_code)]
-fn run_bf_code() {
-
+/// Strips all non-BF characters (all besides + - < > [ ] . ,)
+fn strip_chars(code: String) -> String {
+    let mut result = String::new();
+    for c in code.chars() {
+        if "+-<>[].,".contains(c) {
+            result.push(c);
+        }
+    }
+    result
 }
 
 fn main() {
+    //
     let matches = clap_app!(rainsuck =>
         (version: "0.1.1")
         (author: "cyclowns <cyclowns@protonmail.ch>")
         (about: "Brainfuck interpreter for rust")
-        (@arg FILE: -f --file "Tells rainsuck that your input is a file")
-        (@arg STRING: -s --string "Tells rainsuck that your input is a string")
-        (@arg INPUT: +required "Brainfuck code to be interpreted, either a file or string as set by -f or -s")
+        (@arg FILE: -f --file "Tells rainsuck that your input is a file. Otherwise, assumed that it is a string")
+        (@arg INPUT: +required "Brainfuck code to be interpreted, either a file or string as set by -f")
     ).get_matches();
+
+    if matches.is_present("FILE") {
+        // get filepath from input + check if it is real and is a file
+        let fpath = matches.value_of("INPUT").unwrap();
+        let rpath = Path::new(fpath);
+
+        if rpath.is_file() {
+            let mut file = File::open(rpath).unwrap();
+            let mut contents = String::new();
+            file.read_to_string(&mut contents).unwrap();
+            evaluate_brainfuck(contents);
+        }
+    } else {
+        println!("no file :(");
+    }
 }
 
